22095f95fa3935f539265f40ab7d0caad512fd69
Rishudas12 <rishudas425@gmail.com>
2025-04-06 22:51:54 +0530

    new commit

---
+++ app/src/main/java/com/example/app1/MainActivity113.java
@@ -0,0 +1,30 @@
+package com.example.app1;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.github.barteksc.pdfviewer.PDFView;
+
+public class MainActivity113 extends AppCompatActivity {
+    PDFView pdfview101;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main113);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        pdfview101 = findViewById(R.id.pdfview101);
+        pdfview101.fromAsset("soc17.pdf")
+                .load();
+    }
+}
---
+++ app/src/main/java/com/example/app1/MainActivity116.java
@@ -0,0 +1,31 @@
+package com.example.app1;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.github.barteksc.pdfviewer.PDFView;
+
+public class MainActivity116 extends AppCompatActivity {
+    PDFView pdfview104;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main116);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        pdfview104 = findViewById(R.id.pdfview104);
+        pdfview104.fromAsset("soc20.pdf")
+                .load();
+
+    }
+}
