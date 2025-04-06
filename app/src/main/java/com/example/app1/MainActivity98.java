ace4cd9674c6c85a215292e4284a68bafa67c986
Rishudas12 <rishudas425@gmail.com>
2025-04-06 22:23:40 +0530

    new commit

---
+++ app/src/main/java/com/example/app1/MainActivity98.java
@@ -0,0 +1,31 @@
+package com.example.app1;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.github.barteksc.pdfviewer.PDFView;
+
+public class MainActivity98 extends AppCompatActivity {
+    PDFView pdfview86;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main98);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        pdfview86 = findViewById(R.id.pdfview86);
+        pdfview86.fromAsset("soc2.pdf")
+                .load();
+
+    }
+}
