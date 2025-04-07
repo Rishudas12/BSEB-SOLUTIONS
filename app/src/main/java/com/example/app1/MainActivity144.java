bfb91e4418e0372727e4609f52ebbff2d0b331b1
Rishudas12 <rishudas425@gmail.com>
2025-04-07 16:18:08 +0530

    new commit

--- app/src/main/java/com/example/app1/MainActivity144.java
+++ app/src/main/java/com/example/app1/MainActivity144.java
@@ -19,6 +19,15 @@ public class MainActivity144 extends AppCompatActivity {
     private Button button200;
     private Button button201;
     private Button button202;
+    private Button button203;
+    private Button button204;
+    private Button button205;
+    private Button button206;
+    private Button button207;
+    private Button button208;
+    private Button button209;
+    private Button button210;
+    private Button button211;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -86,6 +95,79 @@ public class MainActivity144 extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        button203 = findViewById(R.id.button203);
+        button203.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity144.this,MainActivity152.class);
+                startActivity(intent);
+            }
+        });
+        button204 = findViewById(R.id.button204);
+        button204.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity144.this,MainActivity153.class);
+                startActivity(intent);
+            }
+        });
+        button205 = findViewById(R.id.button205);
+        button205.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity144.this,MainActivity154.class);
+                startActivity(intent);
+            }
+        });
+        button206 = findViewById(R.id.button206);
+        button206.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity144.this,MainActivity155.class);
+                startActivity(intent);
+            }
+        });
+        button207 = findViewById(R.id.button207);
+        button207.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity144.this,MainActivity156.class);
+                startActivity(intent);
+            }
+        });
+        button208 = findViewById(R.id.button208);
+        button208.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity144.this,MainActivity157.class);
+                startActivity(intent);
+            }
+        });
+        button209 = findViewById(R.id.button209);
+        button209.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity144.this,MainActivity158.class);
+                startActivity(intent);
+            }
+        });
+        button210 = findViewById(R.id.button210);
+        button210.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity144.this,MainActivity159.class);
+                startActivity(intent);
+            }
+        });
+        button211 = findViewById(R.id.button211);
+        button211.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity144.this,MainActivity160.class);
+                startActivity(intent);
+            }
+        });
+
 
     }
 }
---
+++ app/src/main/java/com/example/app1/MainActivity158.java
@@ -0,0 +1,31 @@
+package com.example.app1;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.github.barteksc.pdfviewer.PDFView;
+
+public class MainActivity158 extends AppCompatActivity {
+    PDFView pdfview142;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main158);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        pdfview142 = findViewById(R.id.pdfview142);
+        pdfview142.fromAsset("booksci14.pdf")
+                .load();
+
+    }
+}
