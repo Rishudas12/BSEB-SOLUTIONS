5cc33e8ba89a4e052c364284092f5d174d4723fe
Rishudas12 <rishudas425@gmail.com>
2025-04-07 23:25:53 +0530

    new commit

--- app/src/main/java/com/example/app1/MainActivity.java
+++ app/src/main/java/com/example/app1/MainActivity.java
@@ -2,17 +2,10 @@ package com.example.app1;
 
 import android.content.Intent;
 import android.os.Bundle;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -22,8 +15,7 @@ public class MainActivity extends AppCompatActivity {
     private Button button3;
     private Button button4;
     private Button button5;
-    private EditText edittext;
-    private TextView textview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -69,6 +61,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
     }
+
 }
