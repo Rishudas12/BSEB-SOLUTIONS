77c439ea6f8ce394314af49ec78b957eabfef789
Rishudas12 <rishudas425@gmail.com>
2025-04-12 08:39:01 +0530

    change commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation(libs.play.services.ads.v2420)
     implementation (libs.android.pdf.viewer)
 }
